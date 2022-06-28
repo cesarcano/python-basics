90f9e4a40058480b9d9f4ea94a69583b29d554a3
Sauljmnzt <saul_jt11@outlook.es>
2022-06-28 16:08:46 -0500

    changes

--- for.py
+++ for.py
@@ -1,10 +1,22 @@
 # Crear un programa que reciba N numero de nombres y los ordene alfabéticamente
 # Utilizar Listas y funciones
 
-frase = input("Introduce una frase: ")
-letra = input("Introduce una letra")
-contador = 0
-for i in frase:
-    if i == letra:
-        contador += 1
-print("La letra '%s' aparece %2i veces en la frase '%s'." % (letra, contador, frase))
+listName=[]
+print ("cuantos nombres son")
+total= int(input())
+i = 0
+while i < total:
+    print ("ingrese el nombre", i+1)
+    name = str(input())
+    listName.append(name)
+    i += 1
+listName.sort()
+
+print (listName)
+
+
+
+
+
+
+
