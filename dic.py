d99a5efe586258be293e34b9ddfb9d0aa975c00a
Sauljmnzt <saul_jt11@outlook.es>
2022-06-28 16:53:06 -0500

    lista con diccionarios

---
+++ dic.py
@@ -0,0 +1,22 @@
+listCar = [
+    {
+        "marca" : "bmw",
+        "año" : "2012",
+        "modelo" : "120i",
+        "color" : "blanco"
+    },
+    {
+        "marca" : "honda",
+        "año" : "2016",
+        "modelo" : "fit",
+        "color" : "azul"
+    },
+    {
+        "marca" : "volkswagen",
+        "año" : "2014",
+        "modelo" : "mk5",
+        "color" : "cafe"
+    }
+]
+print(listCar)
+
