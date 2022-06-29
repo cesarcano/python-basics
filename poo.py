26f33db3aa32422b21302efadb7d689464e32a44
Sauljmnzt <saul_jt11@outlook.es>
2022-06-29 13:35:49 -0500

    changes

--- poo.py
+++ poo.py
@@ -3,28 +3,22 @@ from mailbox import NoSuchMailboxError
 
 class alumno:
 
-    def __init__(self, nombre, nota):
-        self.nombre = nombre
-        self.nota = nota
+    def __init__(self):
+        self.nombre = input("cual es tu nombre")
+        self.nota = input("cual es tu calificaicon final")
     
     def imprimir(self):
         print("nombre del alumno", self.nombre)
-        print("nota", self.nota)
+        print
+        ("nota", self.nota)
 
     def resultado (self):
-        if self.nota <7:
-            print ("el alumno reprobo")
+        if int(self.nota) <7:
+            print ("El alumno reprobo")
         else:
-            print("el alumno aprobo")
+            print("El alumno aprobo")
 
-alumno1 = alumno("diego",4)
-alumno2 = alumno("saul",10)
+alumno1 = alumno()
 
 alumno1.imprimir()
 alumno1.resultado()
-
-alumno2.imprimir()
-alumno2.resultado()
-
-
-     
